958598b29053dd8fc72e9ee7621e356f8a473ffb
questcc <137509752+questcc@users.noreply.github.com>
2023-06-24 11:54:53 +0530

    - Added application functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,48 @@ import { FinanceTrackerService } from './services/finance-tracker.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Persoanl-Finance-Tracker';
-  constructor(private financeTrackerService: FinanceTrackerService) {
-    
+  newEntry: {
+    title: string;
+    amount: number;
+  } = {
+    title: '',
+    amount: 0
+  };
+
+  transactions$ = this.financeTrackerService.transactions;
+
+  balance$ = this.financeTrackerService.balance;
+
+  constructor(private financeTrackerService: FinanceTrackerService) {}
+
+  addExpense() {
+    this.financeTrackerService.recordTransaction('Expense', -100);
+  }
+
+  addIncome() {
+    this.financeTrackerService.recordTransaction('Income', 200);
+  }
+
+  addTransaction(isIncome: boolean) {
+    this.financeTrackerService.recordTransaction(this.newEntry.title, isIncome ? this.newEntry.amount : -this.newEntry.amount);
+  }
+
+  deleteTransaction(id: number) {
+    this.financeTrackerService.deleteTransaction(id);
+  }
+
+  getClass(amount: number) {
+    return {
+      'is-success': Number(amount) > 0,
+      'is-danger': Number(amount) < 0
+    };
+  }
+
+  getValue(amount: number) {
+    return ((amount > 0 ? amount : -amount)).toFixed(2);
+  }
+
+  getBalance() {
+    return this.getValue(this.balance$.getValue());
   }
 }
--- src/app/services/finance-tracker.service.ts
+++ src/app/services/finance-tracker.service.ts
@@ -1,18 +1,53 @@
 import { Injectable } from '@angular/core';
+import { FinanceData } from './models/finance-data';
 import { BehaviorSubject } from 'rxjs';
+import { Transaction } from './models/transaction';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FinanceTrackerService {
+  private financeData: FinanceData;
 
-  balance: BehaviorSubject<number>;
+  balance: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+
+  transactions: BehaviorSubject<Transaction[]> = new BehaviorSubject<Transaction[]>([]);
 
   constructor() {
-    const balance = localStorage.getItem('balance');
-    console.log(balance);
-    this.balance = new BehaviorSubject<number>(0);
+    const savedFinanceData = localStorage.getItem('financeData');
+    if (savedFinanceData === null) {
+      this.financeData = this.createNewFinanceData();
+    } else {
+      this.financeData = JSON.parse(savedFinanceData);
+    }
+    this.financeData = savedFinanceData === null ? this.createNewFinanceData(): JSON.parse(savedFinanceData);
+    this.saveFinanceData();
   }
 
+  createNewFinanceData() {
+    return {
+      transactions: []
+    };
+  }
 
+  recordTransaction(title: string, amount: number) {
+    const newTransaction = {
+      id: this.financeData.transactions.length,
+      title,
+      amount
+    };
+    this.financeData.transactions.push(newTransaction);
+    this.saveFinanceData();
+  }
+
+  saveFinanceData() {
+    this.balance.next(this.financeData.transactions.reduce((acc, curr) => { return acc + curr.amount; }, 0));
+    this.transactions.next(this.financeData.transactions);
+    localStorage.setItem('financeData', JSON.stringify(this.financeData));
+  }
+
+  deleteTransaction(id: number) {
+    this.financeData.transactions = this.financeData.transactions.filter(transaction => transaction.id !== id);
+    this.saveFinanceData();
+  }
 }
